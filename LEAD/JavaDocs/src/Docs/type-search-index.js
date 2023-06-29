62a80c2e47d8db0ee88522ca26d5289c883c99d0
giovanavi <72512042+giovanavi@users.noreply.github.com>
2023-06-29 10:23:55 -0300

    Delete LEAD directory

--- LEAD/JavaDocs/src/Docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.empresa.rh","l":"ControleRH"},{"p":"com.empresa.rh","l":"Funcionario"}];updateSearchResults();
